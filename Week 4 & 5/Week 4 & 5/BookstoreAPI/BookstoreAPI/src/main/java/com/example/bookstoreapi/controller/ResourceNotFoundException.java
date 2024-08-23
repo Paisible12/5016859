30a13a5d8e96caa0121cbeaa2b4d128f599b8581
Maitri Samanta <130774345+Paisible12@users.noreply.github.com>
2024-08-23 18:06:17 +0530

    Delete Week 4 & 5 directory

--- Week 4 & 5/Week 4 & 5/BookstoreAPI/BookstoreAPI/src/main/java/com/example/bookstoreapi/controller/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.bookstoreapi.controller;
-
-@SuppressWarnings("serial")
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
